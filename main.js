10b86a26b4f3ee4778eb48e65233b600d7538ec4
sebastian <sebastianjesus.serrano@gracia.lasalle.cat>
2026-04-18 21:01:33 +0200

    feat: inicio del proyecto

---
+++ main.js
@@ -0,0 +1,8 @@
+const btnAdd = document.getElementById('add')
+const btnDelete = document.getElementById('delete')
+const btnEdit = document.getElementById('edit')
+const activiades = document.getElementById('actividades')
+
+btnAdd.addEventListener('click', () =>{
+
+})
