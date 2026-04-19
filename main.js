84ce6f585d1856348a947d8e28e8cb33eabc9472
sebastian <sebastianjesus.serrano@gracia.lasalle.cat>
2026-04-19 18:39:50 +0200

    feat: minimamente funcional

--- main.js
+++ main.js
@@ -4,5 +4,17 @@ const btnEdit = document.getElementById('edit')
 const activiades = document.getElementById('actividades')
 
 btnAdd.addEventListener('click', () =>{
-
+    let tarea = document.createElement("li");
+    tarea.classList.add("act")
+    tarea.innerText = "hola"
+    tarea.dataset.estado = "No_hecha"
+    activiades.appendChild(tarea);
 })
+
+activiades.addEventListener('click', (e) => {
+    const el = e.target.closest(".act")
+    if (!el) return
+    el.dataset.estado = el.dataset.estado == "No_hecha" ? "hecha" : "No_hecha"
+})
+
+
